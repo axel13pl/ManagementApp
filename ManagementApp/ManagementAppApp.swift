3024a136f229bf9dee81abd468403e8666471f9b
Aleksander Gajewski <aleksandergajewski@icloud.com>
2024-07-07 09:25:01 +0200

    Initial Commit

---
+++ ManagementApp/ManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManagementAppApp.swift
+//  ManagementApp
+//
+//  Created by Aleksander Gajewski on 07/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ManagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ManagementAppTests/ManagementAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ManagementAppTests.swift
+//  ManagementAppTests
+//
+//  Created by Aleksander Gajewski on 07/07/2024.
+//
+
+import Testing
+@testable import ManagementApp
+
+struct ManagementAppTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
